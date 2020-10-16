9bf00a231ea834acfa1df65682a3cd926ecd82f1
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-16 16:34:32 -0700

    added a drop down list to view machines and engineers on homepage

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/")]
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db ;
+    }
     public ActionResult Index()
     {
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
+      List<Engineer> model = _db.Engineers.ToList();
+      List<Machine> model2 = _db.Machines.ToList();
       return View();
+    }
+    [HttpPost]
+    public ActionResult Index(Machine machine, int EngineerId)
+    {
+      if (EngineerId != 0)
+      {
+      _db.MachineEngineer.Add(new MachineEngineer() { EngineerId = EngineerId, MachineId = machine.MachineId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", "Engineer", new { id = machine.MachineId});
     }
   }
 }
