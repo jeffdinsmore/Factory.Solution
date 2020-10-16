d626cd1c003f08473b31267dba783b612e16b35c
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-16 14:40:23 -0700

    added header, footer and updated home index page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,25 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
-using Factory.Models;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-    private readonly FactoryContext _db;
-
-    public HomeController(FactoryContext db)
-    {
-      _db = db ;
-    }
-    
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Engineer> model = _db.Engineers.ToList();
-      return View(model);
+      return View();
     }
   }
 }
